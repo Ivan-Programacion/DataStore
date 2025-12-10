e02b842d7b353b67acae300dbdcc55b6481cf34d
Iván Vázquez Martín <ivan.vazquez@live.u-tad.com>
2025-12-10 13:24:35 +0100

    Initial commit

---
+++ app/src/main/java/com/example/datastore/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.datastore
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.datastore.ui.theme.DataStoreTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DataStoreTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    UserNameScrean(innerPadding)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun UserNameScrean(paddingValues: PaddingValues) {
+    Column(
+        Modifier
+            .padding(paddingValues)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("User: ")
+        TextField("", {})
+        Button({}) { Text("Guardar usuario!") }
+    }
+
+}
---
+++ app/src/main/java/com/example/datastore/UserViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.datastore
+
+import androidx.lifecycle.ViewModel
+
+class UserViewModel: ViewModel() {
+
+}
